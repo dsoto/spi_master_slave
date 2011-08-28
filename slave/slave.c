d32919f28dd0e08a43d7a7547d7794c8ba4ac822
Daniel Soto <danielrsoto@gmail.com>
2011-08-27 21:16:26 -0400

    adds interrupt driven architecture to slave code

--- slave/slave.c
+++ slave/slave.c
@@ -4,27 +4,34 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
+// todo: create internal counter and use interrupt architecture
 
 int main ()
 {
     // set MISO as output
     DDRB = (1 << 4);
     // enable SPI
-    SPCR = (1 << SPE);
+    SPCR |= (1 << SPE);
+    // enable SPI interrupt
+    SPCR |= (1 << SPIE);
+    // enable global interrupts
+    SREG = (1 << 7);
 
     for(;;)
     {
-        while(!(SPSR & (1 << SPIF)));
-        int request = SPDR;
-        if (request == 0x01) {
-            SPDR = request << 1;
-        }
-        if (request == 0x02) {
-            SPDR = request << 2;
-        }
-
     }
 
 }
 
+ISR(SPI_STC_vect) {
+    int request = SPDR;
+    if (request == 0x01) {
+        SPDR = request << 1;
+    }
+    if (request == 0x02) {
+        SPDR = request << 2;
+    }
+
+}
